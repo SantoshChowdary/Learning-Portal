9086f26c34e61e0e60a50c7f7c299f6be7916853
SantoshChowdary <santoshssk923@gmail.com>
2023-10-16 20:27:08 +0530

    started header

---
+++ src/components/Header/main/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./index.css"
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <img src="" alt="nxtwave-header-logo" className="nxtwave-header-logo" />
+        </div>
+        <div>
+            <p>Home</p>
+            <p>Testing</p>
+            <img src="" alt="header-profile-pic" className="header-profile-pic" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
